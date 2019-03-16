2f2da1b28060af895ed99c8f7bd0ae84f08aa77e
Waly <debianw@gmail.com>
2019-03-15 18:21:01 -0600

    first commit

---
+++ server/rooms/game.js
@@ -0,0 +1,63 @@
+//
+const { Room } = require('colyseus')
+// const cluster = require('cluster')
+const uuid = require('uuid/v4')
+
+//
+class Game extends Room {
+
+  // When room is initialied
+  onInit(options) {
+    console.log('Game room init, ', options)
+    this.setState({ messages: [] })
+  }
+
+  // Checks if a new client is allowed to join
+  requestJoin(options, isNew) {
+    // console.log('requestJoin: ', options, ' isNew: ', isNew, ' worker: ', cluster.worker.id)
+    console.log('requestJoin: ', options, ' isNew: ', isNew)
+    return true
+  }
+
+  // When client successfully join the room
+  onJoin(client, options, auth) {
+    console.log(`Client ${client.id} joined!`)
+    this.sendMessage(`Player ${client.id} joined`)
+  }
+
+  // When a client sends a message
+  onMessage(client, message) {
+    // console.log(`Client ${client.id} sends: `, message, ' in worker: ', cluster.worker.id)
+    console.log(`Client ${client.id} sends: `, message)
+    this.sendMessage(message)
+  }
+
+  sendMessage(message) {
+    this.state.messages.push({ id: uuid(), message })
+  }
+
+  // When a client leaves the room
+  async onLeave(client, consented) {
+    console.log('IS CONSENTED ?', consented)
+
+    try {
+      if (consented) throw new Error('just close!')
+
+      await this.allowReconnection(client, 5)
+      console.log('CLIENT RECONNECTED: ', client.sessionId)
+    } catch(err) {
+      console.log(err)
+      this.sendMessage(`${client.id} left.`)
+      console.log('ChatRoom: ', client.id, ' left!')
+    }
+  }
+
+  // Cleanup callback, called after there are no more clients in the room
+  onDispose() {
+    console.log('No more clients in the room')
+  }
+
+}
+
+//
+module.exports = Game
---
+++ server/service.js
@@ -0,0 +1,62 @@
+//
+const express = require('express')
+const colyseus = require('colyseus')
+const os = require('os')
+const cluster = require('cluster')
+const { createServer } = require('http')
+const GameRoom = require('./rooms/game')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use(express.static(`${__dirname}/./static`))
+
+app.get('/health-check', (req, res) => {
+  res.json({
+    ok: true,
+    worker: cluster.worker.id,
+  })
+})
+
+const gameServer = new colyseus.Server({
+  pingTimeout: 0,
+  server: createServer(app),
+  // presence: new colyseus.MemsharedPresence(),
+})
+
+gameServer.register('game', GameRoom)
+
+gameServer.listen(port)
+console.log(`Worker Game service listening on port ${port}`)
+
+// if (cluster.isMaster) {
+//   // BEGIN ------------------------------------ MASTER ----------------------------------
+//   console.log('Starting master game service')
+//   console.log('Node version: ', process.version)
+
+//   const cpus = os.cpus()
+//   cpus.forEach(cpu => {
+//     cluster.fork()
+//   })
+//   // ENDS ------------------------------------ MASTER ----------------------------------
+// } else {
+//   // BEGIN ------------------------------------ WORKER ----------------------------------
+//   app.get('/', (req, res) => {
+//     res.json({
+//       ok: true,
+//       worker: cluster.worker.id,
+//     })
+//   })
+
+//   const gameServer = new colyseus.Server({
+//     server: createServer(app),
+//     presence: new colyseus.MemsharedPresence(),
+//   })
+
+//   gameServer.register('game', GameRoom)
+
+//   gameServer.listen(port)
+//   console.log(`Worker Game service listening on port ${port}`)
+//   // ENDS ------------------------------------ WORKER ----------------------------------
+// }
+
